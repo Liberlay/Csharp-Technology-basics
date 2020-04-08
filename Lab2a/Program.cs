7902070f383dc143ca8cb804045024cb0da29447
Liberlay <liberlay555@gmail.com>
2020-04-08 17:41:24 +0300

    Lab2a

---
+++ Lab2a/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab2a
+{
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter amount of squares:");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Square[] squares = new Square[k];
+            Random r = new Random();
+            double sumSquare = 0;
+
+            for(int i = 0; i <k; i++)
+            {
+                squares[i] = new Square(Math.Round(r.NextDouble() * 10, 2), "S" + (i + 1).ToString());
+                Console.WriteLine(squares[i].ToString());
+                sumSquare += squares[i].getSquare();
+
+            }
+
+            Console.WriteLine("\nAverage square of " + k + " square is: " + sumSquare / k);
+
+        }
+    }
+
+
+
+
+
+}
---
+++ Lab2a/Square.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2a
+{
+    class Square
+    {
+
+        double side;
+        string title;
+
+        public Square(double side, string title)
+        {
+            this.side = side;
+            this.title = title;
+        }
+
+        public double getDiagonal ()
+        {
+            return Math.Round(Math.Sqrt(2) * this.side, 2); 
+        }
+
+        public double getPerimeter()
+        {
+            return this.side * 4;
+        }
+
+        public double getSquare ()
+        {
+            return Math.Round(Math.Pow(this.side, 2), 2); 
+        }
+
+        public override String ToString()
+        {
+            return
+                "\nSquare:    " + this.title + " (" + this.side + ")" +
+                "\nDiagonal:  " + this.getDiagonal() +
+                "\nPerimeter: " + this.getPerimeter() +
+                "\nSquare:    " + this.getSquare();
+        }
+
+    }
+}
